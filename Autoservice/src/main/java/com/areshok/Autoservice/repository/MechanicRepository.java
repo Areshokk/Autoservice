15e45d15e75e8754a9f94e0187b2adfb3fdc3c64
Yura <67182548+Areshok2@users.noreply.github.com>
2022-02-01 18:43:23 +0200

    update autoservice

--- Autoservice/src/main/java/com/areshok/Autoservice/repository/MechanicRepository.java
+++
@@ -1,9 +0,0 @@
-package com.areshok.Autoservice.repository;
-
-import com.areshok.Autoservice.model.Mechanic;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MechanicRepository extends JpaRepository<Mechanic,Integer> {
-}
--- Autoservice/src/main/java/com/areshok/Autoservice/repository/ModelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.areshok.Autoservice.repository;
-
-import com.areshok.Autoservice.model.Model;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ModelRepository extends JpaRepository<Model, Integer> {
-}
